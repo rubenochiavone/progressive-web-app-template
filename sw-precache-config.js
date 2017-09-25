02adf6756899fd19fac1a76de0dfb2c55da4c12d
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2017-09-24 22:29:08 -0300

    Update dependencies
    Change code according to migration guide

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,22 +1,19 @@
-/* Config for offline caching with Service Worker Precache
-   https://www.polymer-project.org/1.0/toolbox/service-worker
-   https://github.com/GoogleChrome/sw-precache/blob/master/GettingStarted.md */
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
 module.exports = {
-
-  /* cacheId is string used to distinguish the caches created by different web applications
-     that are served off of the same origin and path.
-     While serving completely different sites from the same URL is not likely to be an issue
-     in a production environment, it avoids cache-conflicts when testing various projects
-     all served off of http://localhost. */
-  cacheId: 'progressive-app-v1',
-
-  /* Array of one or more string patterns that will be passed in to glob.
-     All files matching these globs will be automatically precached by the generated service worker. */
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/webcomponents/webcomponentsjs/webcomponents*.js',
+    '/bower_components/webcomponents/webcomponentsjs/custom-elements-es5-adapter.js',
     '/data/locales/**/*.json',
     '/fonts/**/*.*',
     '/images/**/*.*'
